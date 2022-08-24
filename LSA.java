2ab7c2b2b34994ebf8707ec98d67b6e6683e15bb
Maxm326 <111928970+Maxm326@users.noreply.github.com>
2022-08-23 19:02:36 -0500

    Added down payment

--- LSA.java
+++ LSA.java
@@ -4,13 +4,15 @@ import java.util.Scanner;
 public class LSA {
 
     double loanAmount;
+    double downPayment;
     int creditScore;
     int payTerm;
 
-    public LSA(double amount, int score, int term) {
+    public LSA(double amount, double dwnPayment, int score, int term) {
         loanAmount = amount;
         creditScore = score;
         payTerm = term;
+        downPayment = dwnPayment;
     }
 
     public void loanApp() {
@@ -27,13 +29,15 @@ public class LSA {
             interestRate = 4.9;
         }
 
-        double monthlyPay = loanAmount * (1 + (interestRate/100))/ payTerm;
+        double monthlyPay = (loanAmount - downPayment) * (1 + (interestRate/100))/ payTerm;
 
         DecimalFormat LAformat = new DecimalFormat("#.00");
         DecimalFormat MPformat = new DecimalFormat("#.##");
+        DecimalFormat Dpayment = new DecimalFormat("#.##");
 
             System.out.println("Here is your Loan Offer:");
             System.out.println("Loan Amount: $" + LAformat.format(loanAmount));
+            System.out.println("Down Payment: $" + Dpayment.format(downPayment));
             System.out.println("Interest Rate: " + interestRate + "%");
             System.out.println("Repayment Term: " + payTerm + " months");
             System.out.println("Monthly Payment: $" + MPformat.format(monthlyPay));
@@ -43,16 +47,19 @@ public class LSA {
         Scanner loanAmt = new Scanner(System.in);
         Scanner cScore = new Scanner(System.in);
         Scanner pTerm = new Scanner(System.in);
+        Scanner dPay = new Scanner(System.in);
 
         System.out.println("Loan Submission Application:");
         System.out.println("Please enter your Loan Amount ($):");
         double lAmount = loanAmt.nextDouble();
+        System.out.println("What is your Down Payment ($):");
+        double downPay = dPay.nextDouble();
         System.out.println("What is your credit score (0-850):");
         int creditS = cScore.nextInt();
         System.out.println("What is your payment term in months (0-72):");
         int payTm = pTerm.nextInt();
 
-        LSA loanApp1 = new LSA(lAmount,creditS,payTm);
+        LSA loanApp1 = new LSA(lAmount,downPay,creditS,payTm);
         loanApp1.loanApp();
     }
 }
